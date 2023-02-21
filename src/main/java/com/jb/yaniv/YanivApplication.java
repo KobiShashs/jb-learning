087ac3fd6e0202c2719701d21f34bdb8d91fd128
Kobi S <kobi.shasha@gmail.com>
2023-02-22 01:09:26 +0200

    Baby steps

---
+++ src/main/java/com/jb/yaniv/YanivApplication.java
@@ -0,0 +1,13 @@
+package com.jb.yaniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jb/yaniv/YanivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.yaniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
