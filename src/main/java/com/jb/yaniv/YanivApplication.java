69815b2f688f2c901f4fa366b5f6cfe2be8d6a08
Kobi S <kobi.shasha@gmail.com>
2023-02-22 01:10:49 +0200

    Baby steps

--- src/main/java/com/jb/yaniv/YanivApplication.java
+++ src/main/java/com/jb/yaniv/YanivApplication.java
@@ -2,12 +2,20 @@ package com.jb.yaniv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YanivApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YanivApplication.class, args);
+	}
+
+	@GetMapping("yaniv")
+	public String helloYaniv(){
+		return "Hello from Yaniv!";
 	}
 
 }
